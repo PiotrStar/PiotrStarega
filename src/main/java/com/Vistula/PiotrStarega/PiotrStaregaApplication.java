9e76ee2d49eef5784c666b24f0eb869ee254a52d
PiotrStar <pstarega91@gmail.com>
2025-01-11 17:46:22 +0100

    Initial commit

---
+++ src/main/java/com/Vistula/PiotrStarega/PiotrStaregaApplication.java
@@ -0,0 +1,13 @@
+package com.Vistula.PiotrStarega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiotrStaregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiotrStaregaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Vistula/PiotrStarega/PiotrStaregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vistula.PiotrStarega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiotrStaregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
